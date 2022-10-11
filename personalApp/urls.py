d0ec1ce4be9e4b3a91cbc2d5063bfcace7c955e5
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-11 21:59:26 +0300

    personalApp views ans urls

--- personalApp/urls.py
+++ personalApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 
-from .views import DepartmentView
+from .views import DepartmentView,DepartmentPersonalView
 
 urlpatterns = [
     path('', DepartmentView.as_view()),
+    path('department/<str:department>/', DepartmentPersonalView.as_view()),
     
 ]
--- personalApp/views.py
+++ personalApp/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 from rest_framework import status
 from rest_framework import generics
-from personelApp.models import Department, Personal
+from personalApp.models import Department, Personal
 from rest_framework.generics import GenericAPIView,ListCreateAPIView,RetrieveUpdateDestroyAPIView
-from personelApp.serializers import DepartmentPersonalSerializer, DepartmentSerializer, PersonalSerializer
+from personalApp.serializers import DepartmentPersonalSerializer, DepartmentSerializer, PersonalSerializer
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
@@ -15,6 +15,16 @@ class DepartmentView(generics.ListAPIView):
     # permission_classes=[IsAuthenticated]  ##sadece auth olanlar get isteği atabilsin.
 
 
-class DepartmentPersonelView(generics.ListAPIView):
+class DepartmentPersonalView(generics.ListAPIView):
+    serializer_class=DepartmentSerializer
+    queryset=Department.objects.all()
+    # permission_classes=[IsAuthenticated]
+
+    def get_queryset(self):
+        name=self.kwargs['department']
+        return Department.objects.filter(name__iexact=name) #dinamik url yapmış olduk kullanıcı department adına göre şstek atabilecek ve sadece ilgili departmenti sergileyecek 
+        ## __iexact küçük büyük aldırış etmeme metodu
+        ## bu method filterlamak için kullanılıyor
+
 
 
