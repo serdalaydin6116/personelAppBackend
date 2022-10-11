2d3e44bb6dcfe3e64379ad0f0f6f9729398b8524
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-12 00:15:33 +0300

    was created

--- personalApp/signals.py
+++ personalApp/signals.py
@@ -3,10 +3,10 @@ from .models import Personal
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-@receiver(post_save, sender=Personal)
-def is_staffed_user(sender, instance, **kwargs):
+@receiver(post_save,sender=Personal)
+def is_staffed_user(sender,instance,**kwargs):
     if instance.is_staffed:
-        user = User.objects.get(first_name=instance.first_name, last_name=instance.last_name)
+        user= User.objects.get(first_name=instance.first_name,last_name=instance.last_name)
         if user:
             user.is_staff = True
             user.save()
