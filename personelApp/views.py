fbb979fe8abdbff332228351d868523977e6f368
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-11 21:27:40 +0300

    views was created and imports were done

--- personelApp/views.py
+++ personelApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework import generics
+from personelApp.models import Department, Personal
+from rest_framework.generics import GenericAPIView,ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from personelApp.serializers import DepartmentPersonalSerializer, DepartmentSerializer, PersonalSerializer
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+
+
